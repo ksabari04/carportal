cb4e69dffc3c6949ea8e4d88f44c0a66fe422369
ksabari04 <147916315+ksabari04@users.noreply.github.com>
2025-02-23 16:50:28 +0530

    Update config.php

--- carrental/includes/config.php
+++ carrental/includes/config.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
+define('DB_HOST','mysql');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','UserRoot!');
 define('DB_NAME','carrental');
 // Establish database connection.
 try
